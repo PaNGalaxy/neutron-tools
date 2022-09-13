a26f62db9a5584532638868c416b901170e30ba5
Yakubov, Sergey <yakubovs@ornl.gov>
2022-09-13 14:26:24 -0400

    get SNS data via scp

--- tools/neutrons/import_oncat.py
+++ tools/neutrons/import_oncat.py
@@ -2,26 +2,61 @@ import pyoncat
 import argparse
 import os
 import sys
+import scp,paramiko
+import jwt
+import socket
 
 ONCAT_URL = "https://oncat.ornl.gov"
-BUFSIZ = 8192
+SNS_STORAGE_HOST = "https://calvera-test.ornl.gov"
+MAX_PASSWD_STR_LENGTH = 511
+
+token = os.environ["_GALAXY_OIDC_ID_TOKEN"]
 
 def fix_ipts(input_string):
   if not input_string.upper().startswith("IPTS-"):
     return "IPTS-" + input_string
   return input_string
 
-parser = argparse.ArgumentParser();
+def interaction_handler(_title, _instructions, prompt_list):
+  global token
+  resp = []
+  for pr in prompt_list:
+    if pr[0].strip() == "Next:":
+      if len(token) == 0:
+        resp.append('token_end')
+      elif len(token) > MAX_PASSWD_STR_LENGTH:
+        resp.append(token[0:MAX_PASSWD_STR_LENGTH])
+        token = token[MAX_PASSWD_STR_LENGTH:]
+      else:
+        resp.append(token)
+        token = ''
+    else:
+      if len(token) > MAX_PASSWD_STR_LENGTH:
+        resp.append(token[0:MAX_PASSWD_STR_LENGTH])
+        token = token[MAX_PASSWD_STR_LENGTH:]
+  return tuple(resp)
+
+def copy_datafile(input:str,output:str):
+  username,_ = jwt.decode(token, options={"verify_signature": False})['preferred_username'].split("@",1)
+  sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  sock.connect((SNS_STORAGE_HOST, 22))
+  ts = paramiko.Transport(sock)
+  ts.start_client(timeout=10)
+  ts.auth_interactive(username, interaction_handler)
+  scp_client = scp.SCPClient(ts)
+  scp_client.get(remote_path=input,local_path=output)
+
+
+parser = argparse.ArgumentParser()
 parser.add_argument("-i", "--instrument", type=str, required=True, help="Instrument name")
 parser.add_argument("-f", "--facility", type=str, required=True, help="Facility name")
 parser.add_argument("-I", "--ipts", type=str, help="IPTS number")
 parser.add_argument("-r", "--run", type=str, help="Run number")
-args = parser.parse_args();
+parser.add_argument("-o", "--output", type=str, help="Output file")
+
+args = parser.parse_args()
 
 oncat = pyoncat.ONCat(ONCAT_URL)
-
-#experiment = oncat.Experiment.retrieve(args.ipts, facility=args.facility, instrument=args.instrument)
-#print(experiment.get("indexed.run_number.ranges"))
 
 projection = [
   "indexed.run_number",
@@ -31,23 +66,18 @@ projection = [
 
 if not args.ipts:
   experiments = oncat.Experiment.list(facility=args.facility, instrument=args.instrument)
-  for experiment in experiments:
-    if experiment.get("title"):
-      print("%s\t%s\t%s" % (experiment.get("name"), experiment.get("title"), experiment.get("")))
+  with open(args.output, 'w') as f:
+    for experiment in experiments:
+      if experiment.get("title"):
+        f.write("%s\t%s\t%s" % (experiment.get("name"), experiment.get("title"), experiment.get("")))
 elif not args.run:
-  datafiles = oncat.Datafile.list(experiment=fix_ipts(args.ipts), facility=args.facility, instrument=args.instrument, projection=projection, exts=[".nxs.h5"])
-  for datafile in datafiles:
-    print("%s\t%s" % (datafile.get("indexed.run_number"), datafile.get("location")))
+  with open(args.output, 'w') as f:
+    datafiles = oncat.Datafile.list(experiment=fix_ipts(args.ipts), facility=args.facility, instrument=args.instrument, projection=projection, exts=[".nxs.h5"])
+    for datafile in datafiles:
+      f.write("%s\t%s" % (datafile.get("indexed.run_number"), datafile.get("location")))
 else:
   datafiles = oncat.Datafile.list(experiment=fix_ipts(args.ipts), facility=args.facility, instrument=args.instrument, projection=projection, exts=[".nxs.h5"], ranges_q="indexed.run_number:" + args.run)
   if len(datafiles) != 1:
     print("Invalid run number")
     sys.exit(1)
-  with open(datafiles[0].get("location"), 'rb') as f, os.fdopen(sys.stdout.fileno(), 'wb', closefd=False) as stdout:
-    data = f.read(BUFSIZ)
-    while data:
-      stdout.write(data)
-      stdout.flush()
-      data = f.read(BUFSIZ)
-
-
+  copy_datafile(datafiles[0].get("location"),args.output)
