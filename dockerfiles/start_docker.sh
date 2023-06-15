7e6074408dea337406cce08975bed41dfc5cf1d6
Adavita Jain <adavitajain@Adavitas-MacBook-Pro.local>
2023-06-15 10:58:13 -0400

    updated mount directories

--- dockerfiles/start_docker.sh
+++ dockerfiles/start_docker.sh
@@ -1,9 +1,19 @@
  #! /bin/bash
 
 docker run -it -v /var/run/docker.sock:/var/run/docker.sock \
-   -v `pwd`/tools:/galaxy-tools/tools \
-   -v `pwd`/tool_data:/galaxy-tools/tool_data \
-   --workdir /galaxy-tools/tools/neutrons \
+   -v /Users/adavitajain/Downloads/galaxy-tools/tools:/Users/adavitajain/Downloads/galaxy-tools/tools \
    -v /tmp:/tmp \
-dummy-image planemo test --galaxy_root /galaxy --docker --tool_data_path /tmp
+dummy-image
+
+#planemo test --galaxy_root /galaxy --docker --tool_data_path /tmp --no_cleanup fractal.xml
+
+
+
+#docker run -it -v /var/run/docker.sock:/var/run/docker.sock \
+#   -v `pwd`/tools:/galaxy-tools/tools \
+#   -v `pwd`/tool_data:/galaxy-tools/tool_data \
+#   --workdir /galaxy-tools/tools/neutrons \
+#   -v /tmp:/tmp \
+#dummy-image planemo test --galaxy_root /galaxy --docker --tool_data_path /tmp
+
 
