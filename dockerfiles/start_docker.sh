7ee81f1ac1e1c49f7575b5739f664decf1e53695
Adavita Jain <adavitajain@Adavitas-MacBook-Pro.local>
2023-06-15 11:39:04 -0400

    added command to look at test output

--- dockerfiles/start_docker.sh
+++ dockerfiles/start_docker.sh
@@ -1,12 +1,14 @@
  #! /bin/bash
 
-docker run -it -v /var/run/docker.sock:/var/run/docker.sock \
+#docker run -it -v /var/run/docker.sock:/var/run/docker.sock \
+#   -v /Users/adavitajain/Downloads/galaxy-tools/tools:/Users/adavitajain/Downloads/galaxy-tools/tools \
+#   -v /tmp:/tmp \
+#dummy-image planemo test --galaxy_root /galaxy --docker --tool_data_path /tmp --no_cleanup fractal.xml
+
+ docker run -it -v /var/run/docker.sock:/var/run/docker.sock \
    -v /Users/adavitajain/Downloads/galaxy-tools/tools:/Users/adavitajain/Downloads/galaxy-tools/tools \
-   -v /tmp:/tmp \
+   -v /tmp/planemo:/tmp/planemo \
 dummy-image
-
-#planemo test --galaxy_root /galaxy --docker --tool_data_path /tmp --no_cleanup fractal.xml
-
 
 
 #docker run -it -v /var/run/docker.sock:/var/run/docker.sock \
