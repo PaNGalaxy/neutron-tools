6919b8e501aa5d08b2966a0c6e8ca7ae168b9cb8
Jain, Adavita <jaina@ornl.gov>
2023-06-19 18:11:11 +0000

    Delete start_docker.sh

--- dockerfiles/start_docker.sh
+++
@@ -1,8 +0,0 @@
- #! /bin/bash
-
- docker run -it -v /var/run/docker.sock:/var/run/docker.sock \
-   -v /Users/adavitajain/Downloads/galaxy-tools/tools:/Users/adavitajain/Downloads/galaxy-tools/tools \
-   -v /tmp:/tmp \
-dummy-image
-
-#planemo test --galaxy_root /galaxy --docker --tool_data_path /tmp --no_cleanup fractal.xml
