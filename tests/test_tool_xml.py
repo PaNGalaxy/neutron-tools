9186db264366d3950b20bf30328e838e482b7115
John Duggan <dugganjw@ornl.gov>
2026-04-20 22:19:47 -0400

    Initial set of passing tests

--- tests/test_tool_xml.py
+++ tests/test_tool_xml.py
@@ -1,10 +1,44 @@
 import subprocess
 import sys
 from itertools import chain
+from typing import Any, Dict, List
 
 import pytest
 from galaxy.tool_util.parser import get_tool_source
+from galaxy.tool_util.parser.output_objects import ToolOutputCollection
 from galaxy.util.template import fill_template
+
+
+def _parse_inputs(input_sources: Any) -> Dict[str, Any]:
+    if input_sources is None:
+        return {}
+
+    inputs: Dict[str, Any] = {}
+    for source in chain.from_iterable(input_sources):
+        try:
+            key = source.parse_name()
+        except Exception:
+            continue
+
+        nested_sources = [source.parse_nested_inputs_source().parse_input_sources()]
+        conditional_sources = [
+            conditional_source[1].parse_input_sources()
+            for conditional_source in source.parse_when_input_sources()
+        ]
+        if nested_sources == [[]] and conditional_sources == []:
+            inputs[key] = "__ndip_testing_value"
+        if nested_sources != [[]]:
+            if source.input_type == "repeat":
+                inputs[key] = [_parse_inputs(nested_sources)]
+            else:
+                inputs[key] = _parse_inputs(nested_sources)
+        if conditional_sources != []:
+            if inputs[key] == "__ndip_testing_value":
+                inputs[key] = _parse_inputs(conditional_sources)
+            else:
+                inputs[key] = inputs[key] | _parse_inputs(conditional_sources)
+
+    return inputs
 
 
 def _check_planemo_linter(xml_path: str) -> None:
@@ -25,14 +59,20 @@ def _check_tool_command(xml_path: str) -> None:
     outputs = tool_source.parse_outputs(None)[0].keys()
 
     # Parse tool inputs
-    input_pages = tool_source.parse_input_pages()
-    input_sources = [page.parse_input_sources() for page in input_pages.page_sources]
-    inputs = [input.parse_name() for input in chain.from_iterable(input_sources)]
+    page_sources = tool_source.parse_input_pages().page_sources
+    inputs = _parse_inputs([page.parse_input_sources() for page in page_sources])
 
     # Test if we can build a valid command from the cheetah template
-    fill_template(
-        command, dict.fromkeys(inputs, 0) | dict.fromkeys(outputs, 0), retry=0
-    )
+    try:
+        fill_template(
+            command,
+            inputs | dict.fromkeys(outputs, "test"),
+            retry=0,
+        )
+    except Exception as exc:
+        # These will get flagged but are expected for input files.
+        if "'element_identifier'" not in str(exc) and "'name'" not in str(exc):
+            raise
 
 
 def _check_missing_output(xml_path: str) -> None:
@@ -40,7 +80,14 @@ def _check_missing_output(xml_path: str) -> None:
 
     # Parse tool command and outputs
     command = tool_source.parse_command()
-    outputs = tool_source.parse_outputs(None)[0].keys()
+
+    # Unlike in fill_template, we need to skip collections here
+    output_items = tool_source.parse_outputs(None)[0]
+    outputs: List[str] = []
+    for key, value in output_items.items():
+        if isinstance(value, ToolOutputCollection):
+            continue
+        outputs.append(key)
 
     for output in outputs:
         if f"${output}" not in command:
