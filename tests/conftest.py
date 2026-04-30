52687a1ca1c6f2bea69ab6fb1baddb97d0033276
John Duggan <dugganjw@ornl.gov>
2026-04-30 14:28:11 -0400

    Fix interactive tool tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,16 +3,17 @@ from pathlib import Path
 
 
 def pytest_generate_tests(metafunc) -> None:
-    tool_paths = []
-    for root, _, files in os.walk("tools"):
-        for name in files:
-            fname = os.path.join(root, name)
-            path = Path(fname)
-            if path.suffix == ".xml":
-                tool_paths.append(str(path))
+    if "xml_path" in metafunc.fixturenames:
+        tool_paths = []
+        for root, _, files in os.walk("tools"):
+            for name in files:
+                fname = os.path.join(root, name)
+                path = Path(fname)
+                if path.suffix == ".xml":
+                    tool_paths.append(str(path))
 
-    metafunc.parametrize(
-        "xml_path",
-        tool_paths,
-        ids=[os.path.basename(tool_path) for tool_path in tool_paths],
-    )
+        metafunc.parametrize(
+            "xml_path",
+            tool_paths,
+            ids=[os.path.basename(tool_path) for tool_path in tool_paths],
+        )
