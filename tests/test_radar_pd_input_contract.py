b6bab956aabad30f1f4e79d90f691a0ee57bafb9
Yadav <ly6@ornl.gov>
2026-08-10 20:48:57 -0400

    Require explicit RADAR-PD input selection

--- tests/test_radar_pd_input_contract.py
+++ tests/test_radar_pd_input_contract.py
@@ -48,6 +48,14 @@ def test_analyze_keeps_scientific_inputs_common_to_both_routes() -> None:
 
 def test_analyze_diffraction_picker_excludes_non_pattern_artifacts() -> None:
     root = _root("radar_pd_analyze.xml")
+    source = root.find(
+        "./inputs/section[@name='data_inputs']/conditional[@name='input_source']"
+        "/param[@name='source_kind']"
+    )
+    assert source is not None
+    selected_source = source.find("./option[@selected='true']")
+    assert selected_source is not None and selected_source.attrib.get("value") == "choose"
+
     data = root.find(
         "./inputs/section[@name='data_inputs']/conditional[@name='input_source']"
         "/when[@value='history']/param[@name='diffraction_pattern']"
@@ -57,6 +65,8 @@ def test_analyze_diffraction_picker_excludes_non_pattern_artifacts() -> None:
 
     assert accepted == {"tabular", "xml"}
     assert accepted.isdisjoint({"data", "yaml", "json", "cif", "zip", "html"})
+    data_validator = data.find("./validator[@type='expression']")
+    assert data_validator is not None and ".xrdml" in (data_validator.text or "")
 
     instrument = root.find(
         "./inputs/section[@name='data_inputs']/conditional[@name='input_source']"
@@ -64,6 +74,8 @@ def test_analyze_diffraction_picker_excludes_non_pattern_artifacts() -> None:
         "/when[@value='uploaded']/param[@name='instrument_file']"
     )
     assert instrument is not None and instrument.attrib.get("format") == "txt"
+    instrument_validator = instrument.find("./validator[@type='expression']")
+    assert instrument_validator is not None and ".instprm" in (instrument_validator.text or "")
 
 
 def test_reusable_configuration_uses_the_same_route_contract() -> None:
