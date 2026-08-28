c1c886ce1c13020f5ea7f8433bae75d6a49429ec
Yadav <ly6@ornl.gov>
2026-08-27 21:27:33 -0400

    Deploy RADAR-PD NOVA 0.3.77

--- tests/test_radar_pd_input_contract.py
+++ tests/test_radar_pd_input_contract.py
@@ -362,11 +362,11 @@ def test_nova_interactive_uses_the_smoke_tested_release_image() -> None:
     root = _root("radar_pd_nova.xml")
     container = root.find("./requirements/container")
 
-    assert root.attrib.get("version") == "0.3.76"
+    assert root.attrib.get("version") == "0.3.77"
     assert container is not None
     assert container.text == (
         "ghcr.io/lalityadav07/impurity_detection_gsas_ver6:"
-        "nova-b745ab0fe5d565a364c59523f0044ddac510577a"
+        "nova-e9cd83525881ed15c7306969074ddc824b1965f3"
     )
 
 
